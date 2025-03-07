a7893a63d081265bb8ebbd8fe16e0abeefeecf0d
mart3205 <martin.hernandez@rackspace.com>
2025-03-07 09:09:59 -0600

    Ibarra url

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "http://54.244.42.51:5000";
+const apiBaseUrl = "http://35.89.54.205:5000";
 
 // Function to display messages in the chat area
 function displayMessage(content, isUserMessage = true) {
